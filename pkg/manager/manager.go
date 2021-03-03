a0e80d335225b57c24ba3840924f829b845ad96e
Sonny Garcia <sonnygarcia@icloud.com>
2021-03-02 17:04:30 -0700

    fixes linting issue

--- pkg/manager/manager.go
+++ pkg/manager/manager.go
@@ -1,10 +1,10 @@
 package manager
 
 import (
-	// Import all Kubernetes client auth plugins (e.g. Azure, GCP, OIDC, etc.)
-	// to ensure that exec-entrypoint and run can make use of them.
 	"os"
 
+	// Import all Kubernetes client auth plugins (e.g. Azure, GCP, OIDC, etc.)
+	// to ensure that exec-entrypoint and run can make use of them.
 	_ "k8s.io/client-go/plugin/pkg/client/auth"
 
 	"k8s.io/apimachinery/pkg/runtime"
