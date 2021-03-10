f7312735b2e617c5676ab8923a28b1236db117e0
Bernhard (Sonny) Köhler <sonnygarcia@icloud.com>
2021-03-09 20:40:12 -0700

    Trying out codecov gh action (#11)
    * trying out codecov gh action
    
    * removes codecov-action verbose flag
    
    normal output is informative enough
    
    * adds metadata tests
    
    used to inspect coverage report behavior
    
    * adds docs to metadata.go consts and funcs
    
    * adds RayClusterNetworkPolicy type
    
    this will ultimately replace the existing, ungrouped netpol options
    inside the RayClusterSpec. add this now to trigger a change to generated
    files and fix coverage report

--- pkg/resources/metadata.go
+++ pkg/resources/metadata.go
@@ -1,26 +1,33 @@
 package resources
 
 const (
-	ApplicationNameLabelKey      = "app.kubernetes.io/name"
-	ApplicationInstanceLabelKey  = "app.kubernetes.io/instance"
-	ApplicationVersionLabelKey   = "app.kubernetes.io/version"
+	// ApplicationNameLabelKey indicates the name of the application.
+	ApplicationNameLabelKey = "app.kubernetes.io/name"
+	// ApplicationInstanceLabelKey indicates a unique name identifying the instance of an application.
+	ApplicationInstanceLabelKey = "app.kubernetes.io/instance"
+	// ApplicationVersionLabelKey indicates the current version of the application.
+	ApplicationVersionLabelKey = "app.kubernetes.io/version"
+	// ApplicationComponentLabelKey indicates the component within the architecture of an application.
 	ApplicationComponentLabelKey = "app.kubernetes.io/component"
+	// ApplicationManagedByLabelKey indicates the tool being used to manage the operation of an application.
 	ApplicationManagedByLabelKey = "app.kubernetes.io/managed-by"
-
-	ManagerLabelValue = "distributed-compute-operator"
-
+	// ApplicationManagedByLabelValue is the specific tool being used to manage applications created by this project.
+	ApplicationManagedByLabelValue = "distributed-compute-operator"
+	// DescriptionAnnotationKey can be used to add extra information to a Kubernetes object via its annotations.
 	DescriptionAnnotationKey = "distributed-compute.dominodatalab.com/description"
 )
 
+// MetadataLabels returns a map used to label Kubernetes resources.
 func MetadataLabels(name, instance, version string) map[string]string {
 	return map[string]string{
 		ApplicationNameLabelKey:      name,
 		ApplicationInstanceLabelKey:  instance,
 		ApplicationVersionLabelKey:   version,
-		ApplicationManagedByLabelKey: ManagerLabelValue,
+		ApplicationManagedByLabelKey: ApplicationManagedByLabelValue,
 	}
 }
 
+// MetadataLabelsWithComponent returns a map used to label Kubernetes resources that act as unique components.
 func MetadataLabelsWithComponent(name, instance, version, component string) map[string]string {
 	labels := MetadataLabels(name, instance, version)
 	labels[ApplicationComponentLabelKey] = component
@@ -28,6 +35,8 @@ func MetadataLabelsWithComponent(name, instance, version, component string) map[
 	return labels
 }
 
+// SelectorLabels returns a map used to select Kubernetes objects that have
+// been labeled with output from MetadataLabels.
 func SelectorLabels(name, instance string) map[string]string {
 	return map[string]string{
 		ApplicationNameLabelKey:     name,
@@ -35,6 +44,8 @@ func SelectorLabels(name, instance string) map[string]string {
 	}
 }
 
+// SelectorLabelsWithComponent returns a map used to select Kubernetes objects
+// that have been labeled with output from MetadataLabelsWithComponent.
 func SelectorLabelsWithComponent(name, instance, component string) map[string]string {
 	labels := SelectorLabels(name, instance)
 	labels[ApplicationComponentLabelKey] = component
--- pkg/resources/metadata_test.go
+++ pkg/resources/metadata_test.go
@@ -1,1 +1,53 @@
 package resources
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMetadataLabels(t *testing.T) {
+	actual := MetadataLabels("my-app", "inst", "v1.0.0")
+	expected := map[string]string{
+		"app.kubernetes.io/name":       "my-app",
+		"app.kubernetes.io/instance":   "inst",
+		"app.kubernetes.io/version":    "v1.0.0",
+		"app.kubernetes.io/managed-by": "distributed-compute-operator",
+	}
+
+	assert.Equal(t, expected, actual)
+}
+
+func TestMetadataLabelsWithComponent(t *testing.T) {
+	actual := MetadataLabelsWithComponent("my-app", "inst", "v1.0.0", "comp")
+	expected := map[string]string{
+		"app.kubernetes.io/name":       "my-app",
+		"app.kubernetes.io/instance":   "inst",
+		"app.kubernetes.io/version":    "v1.0.0",
+		"app.kubernetes.io/managed-by": "distributed-compute-operator",
+		"app.kubernetes.io/component":  "comp",
+	}
+
+	assert.Equal(t, expected, actual)
+}
+
+func TestSelectorLabels(t *testing.T) {
+	actual := SelectorLabels("my-app", "inst")
+	expected := map[string]string{
+		"app.kubernetes.io/name":     "my-app",
+		"app.kubernetes.io/instance": "inst",
+	}
+
+	assert.Equal(t, expected, actual)
+}
+
+func TestSelectorLabelsWithComponent(t *testing.T) {
+	actual := SelectorLabelsWithComponent("my-app", "inst", "comp")
+	expected := map[string]string{
+		"app.kubernetes.io/name":      "my-app",
+		"app.kubernetes.io/instance":  "inst",
+		"app.kubernetes.io/component": "comp",
+	}
+
+	assert.Equal(t, expected, actual)
+}
