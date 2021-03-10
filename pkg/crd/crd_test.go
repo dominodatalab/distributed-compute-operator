68c725eee017b9e8cec22dee92600c431749cf57
Sonny Garcia <sonnygarcia@icloud.com>
2021-03-10 16:31:05 -0700

    removes bug where crd create/update were both happening

---
+++ pkg/crd/crd_test.go
@@ -0,0 +1,11 @@
+package crd
+
+import "testing"
+
+func TestApply(t *testing.T) {
+	t.Skip("don't be a scumbag, write me")
+}
+
+func TestDelete(t *testing.T) {
+	t.Skip("don't be a scumbag, write me")
+}
