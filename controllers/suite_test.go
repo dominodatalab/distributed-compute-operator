aa2077ab45024e73fdf2a4adfce6b9aaba0b983f
Bernhard (Sonny) Köhler <sonnygarcia@icloud.com>
2021-03-13 13:18:55 -0700

    Adds initial smoke test for RayClusterReconciler (#15)
    * adds initial smoke test for RayClusterReconciler
    
    * attemps to match latest bug fix go version in ci
    
    * set explicit go version in ci
    
    we won't be able to use latest version until
    https://github.com/actions/setup-go/issues/73 is fixed
    
    * finishes smoke test for full raycluster create
    
    * disables codecov patch testing
    
    this isn't very helpful right now but it could be with the proper
    configuration after we stabilize the project
    
    * adds missing /fixes docs
    
    * reenables project coverage checks
    
    but this time explicitly

--- controllers/suite_test.go
+++ controllers/suite_test.go
@@ -8,6 +8,7 @@ import (
 	. "github.com/onsi/gomega"
 	"k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/client-go/rest"
+	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/envtest"
 	"sigs.k8s.io/controller-runtime/pkg/envtest/printer"
@@ -15,6 +16,7 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 
 	distributedcomputev1alpha1 "github.com/dominodatalab/distributed-compute-operator/api/v1alpha1"
+	"github.com/dominodatalab/distributed-compute-operator/pkg/logging"
 	"github.com/dominodatalab/distributed-compute-operator/test"
 	//+kubebuilder:scaffold:imports
 )
@@ -35,7 +37,7 @@ func TestAPIs(t *testing.T) {
 		[]Reporter{printer.NewlineReporter{}})
 }
 
-var _ = BeforeSuite(func() {
+var _ = BeforeSuite(func(done Done) {
 	logf.SetLogger(zap.New(zap.WriteTo(GinkgoWriter), zap.UseDevMode(true)))
 
 	By("bootstrapping test environment")
@@ -55,10 +57,27 @@ var _ = BeforeSuite(func() {
 
 	//+kubebuilder:scaffold:scheme
 
-	k8sClient, err = client.New(cfg, client.Options{Scheme: scheme.Scheme})
+	k8sManager, err := ctrl.NewManager(cfg, ctrl.Options{
+		Scheme: scheme.Scheme,
+	})
 	Expect(err).NotTo(HaveOccurred())
+
+	err = (&RayClusterReconciler{
+		Client: k8sManager.GetClient(),
+		Scheme: k8sManager.GetScheme(),
+		Log:    logging.New(ctrl.Log.WithName("controllers").WithName("RayCluster")),
+	}).SetupWithManager(k8sManager)
+	Expect(err).ToNot(HaveOccurred())
+
+	go func() {
+		err = k8sManager.Start(ctrl.SetupSignalHandler())
+		Expect(err).ToNot(HaveOccurred())
+	}()
+
+	k8sClient = k8sManager.GetClient()
 	Expect(k8sClient).NotTo(BeNil())
 
+	close(done)
 }, 60)
 
 var _ = AfterSuite(func() {
