ac2bd455aba4e6baffb3d2514a7b6cf8975d8c14
Sonny Garcia <sonnygarcia@icloud.com>
2021-03-23 13:53:38 -0400

    makes development script "deploy" action more robust
    if minikube restarts, then `ssh' spews out a bunch of warnings/errors
    stating the remote host has changed. disabling warnings and known hosts
    entirely is fine for this operation.

--- scripts/development.sh
+++ scripts/development.sh
@@ -20,8 +20,6 @@ function dco::_error {
 }
 
 function dco::minikube_setup() {
-  set -x
-
   if ! minikube profile list 2> /dev/null | grep -q "$MINIKUBE_PROFILE"; then
     dco::_info "Creating minikube cluster"
     minikube start \
@@ -108,7 +106,7 @@ function dco::helm_install() {
   ssh_key=$(minikube ssh-key --profile="$MINIKUBE_PROFILE")
   ip_addr=$(minikube ip --profile="$MINIKUBE_PROFILE")
   latest_tag=$(
-    ssh -o StrictHostKeyChecking=no -i "$ssh_key" docker@"$ip_addr" \
+    ssh  -o LogLevel=ERROR -o StrictHostKeyChecking=no -o GlobalKnownHostsFile=/dev/null -o UserKnownHostsFile=/dev/null -i "$ssh_key" docker@"$ip_addr" \
       "docker image list $IMAGE_NAME:$IMAGE_TAG_PREFIX* --format '{{ .Tag }}|{{ .CreatedAt }}'" | \
         sort -r -t '|' -k 2 | head -n 1 | cut -d '|' -f 1
   )
