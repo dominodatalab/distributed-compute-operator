64bc25853be669b122fa460ebe09a830b397e8ec
Adam Udi <adam.udi@dominodatalab.com>
2021-03-22 19:30:19 -0700

    add some coverage for finalizers

--- controllers/sparkcluster_controller_integration_test.go
+++ controllers/sparkcluster_controller_integration_test.go
@@ -3,8 +3,7 @@ package controllers
 import (
 	"context"
 	"fmt"
-	"time"
-
+	dcv1alpha1 "github.com/dominodatalab/distributed-compute-operator/api/v1alpha1"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	appsv1 "k8s.io/api/apps/v1"
@@ -17,8 +16,7 @@ import (
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/utils/pointer"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-
-	dcv1alpha1 "github.com/dominodatalab/distributed-compute-operator/api/v1alpha1"
+	"time"
 )
 
 var _ = Describe("SparkCluster Controller", func() {
@@ -27,72 +25,24 @@ var _ = Describe("SparkCluster Controller", func() {
 
 		It("should create a functional cluster", func() {
 			ctx := context.Background()
+			name := "functional"
+			createAndBasicTest(ctx, name)
 			timeout := time.Second * 10
-
-			psp := &policyv1beta1.PodSecurityPolicy{
-				ObjectMeta: metav1.ObjectMeta{
-					Name: "it-spark",
-				},
-				Spec: policyv1beta1.PodSecurityPolicySpec{
-					SELinux: policyv1beta1.SELinuxStrategyOptions{
-						Rule: policyv1beta1.SELinuxStrategyRunAsAny,
-					},
-					RunAsUser: policyv1beta1.RunAsUserStrategyOptions{
-						Rule: policyv1beta1.RunAsUserStrategyMustRunAsNonRoot,
-					},
-					SupplementalGroups: policyv1beta1.SupplementalGroupsStrategyOptions{
-						Rule: policyv1beta1.SupplementalGroupsStrategyRunAsAny,
-					},
-					FSGroup: policyv1beta1.FSGroupStrategyOptions{
-						Rule: policyv1beta1.FSGroupStrategyRunAsAny,
-					},
-				},
-			}
-			sparkCluster := &dcv1alpha1.SparkCluster{
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      "it",
-					Namespace: "default",
-				},
-				Spec: dcv1alpha1.SparkClusterSpec{
-					Image: &dcv1alpha1.OCIImageDefinition{
-						Repository: "foo",
-						Tag:        "bar",
-					},
-					Autoscaling: &dcv1alpha1.Autoscaling{
-						MinReplicas:           pointer.Int32Ptr(1),
-						MaxReplicas:           1,
-						AverageCPUUtilization: pointer.Int32Ptr(50),
-					},
-					NetworkPolicy: dcv1alpha1.SparkClusterNetworkPolicy{
-						Enabled: pointer.BoolPtr(true),
-					},
-					Worker: dcv1alpha1.SparkClusterWorker{
-						Replicas: pointer.Int32Ptr(1),
-					},
-					ClusterPort:       7077,
-					DashboardPort:     8265,
-					PodSecurityPolicy: psp.Name,
-				},
-			}
-
-			Expect(k8sClient.Create(ctx, psp)).To(Succeed())
-			Expect(k8sClient.Create(ctx, sparkCluster)).To(Succeed())
-
 			testcases := []struct {
 				desc string
 				name string
 				obj  client.Object
 			}{
-				{"service account", "it-spark", &corev1.ServiceAccount{}},
-				{"head service", "it-spark-master", &corev1.Service{}},
-				{"cluster network policy", "it-spark-cluster", &networkingv1.NetworkPolicy{}},
-				{"client network policy", "it-spark-client", &networkingv1.NetworkPolicy{}},
-				{"dashboard network policy", "it-spark-dashboard", &networkingv1.NetworkPolicy{}},
-				{"pod security policy role", "it-spark", &rbacv1.Role{}},
-				{"pod security policy role binding", "it-spark", &rbacv1.RoleBinding{}},
-				{"horizontal pod autoscaler", "it-spark", &autoscalingv2beta2.HorizontalPodAutoscaler{}},
-				{"head statefulset", "it-spark-master", &appsv1.StatefulSet{}},
-				{"worker statefulset", "it-spark-worker", &appsv1.StatefulSet{}},
+				{"service account", name + "-spark", &corev1.ServiceAccount{}},
+				{"head service", name + "-spark-master", &corev1.Service{}},
+				{"cluster network policy", name + "-spark-cluster", &networkingv1.NetworkPolicy{}},
+				{"client network policy", name + "-spark-client", &networkingv1.NetworkPolicy{}},
+				{"dashboard network policy", name + "-spark-dashboard", &networkingv1.NetworkPolicy{}},
+				{"pod security policy role", name + "-spark", &rbacv1.Role{}},
+				{"pod security policy role binding", name + "-spark", &rbacv1.RoleBinding{}},
+				{"horizontal pod autoscaler", name + "-spark", &autoscalingv2beta2.HorizontalPodAutoscaler{}},
+				{"head statefulset", name + "-spark-master", &appsv1.StatefulSet{}},
+				{"worker statefulset", name + "-spark-worker", &appsv1.StatefulSet{}},
 			}
 			for _, tc := range testcases {
 				By(fmt.Sprintf("Creating a new %s", tc.desc))
@@ -107,13 +57,130 @@ var _ = Describe("SparkCluster Controller", func() {
 					return k8sClient.Get(ctx, key, obj)
 				}, timeout).Should(Succeed())
 			}
+		})
+
+		It("should tear down gracefully", func() {
+			ctx := context.Background()
+			createAndBasicTest(ctx, "teardown")
+			timeout := time.Second * 10
+			cluster := dcv1alpha1.SparkCluster{}
+			Eventually(func() error {
+				return k8sClient.Get(ctx, types.NamespacedName{
+					Namespace: "default",
+					Name: "teardown"},
+					&cluster)
+			}, timeout).Should(Succeed())
+
+			k8sClient.Delete(ctx, &cluster)
+
+			Eventually(func() error {
+				return k8sClient.Get(ctx, types.NamespacedName{
+					Namespace: "default",
+					Name: "teardown"},
+					&cluster)
+			}, timeout).ShouldNot(Succeed())
+
+		})
+
+		It("should add a finalizer", func() {
+			ctx := context.Background()
+			name := "finalizer"
+			timeout := time.Second * 10
+			createAndBasicTest(ctx, name)
 			cluster := dcv1alpha1.SparkCluster{}
 			Expect(k8sClient.Get(ctx, types.NamespacedName{
 				Namespace: "default",
-				Name:      "it",
+				Name:      name,
 			}, &cluster)).Should(Succeed())
-			Expect(len(cluster.Finalizers)).Should(Equal(1))
-			Expect(cluster.Finalizers[0]).Should(Equal(SparkFinalizerName))
+			Eventually(func () bool {
+				k8sClient.Get(ctx, types.NamespacedName{
+					Namespace: "default",
+					Name:      name,
+				}, &cluster)
+				return len(cluster.Finalizers) == 1 && cluster.Finalizers[0] == SparkFinalizerName
+			}, timeout).Should(BeTrue())
+		})
+
+		It("should delete the finalizer", func() {
+			ctx := context.Background()
+			name := "finalizer-delete"
+			timeout := time.Second * 10
+			createAndBasicTest(ctx, name)
+			cluster := dcv1alpha1.SparkCluster{}
+			Expect(k8sClient.Get(ctx, types.NamespacedName{
+				Namespace: "default",
+				Name:      name,
+			}, &cluster)).Should(Succeed())
+
+			cluster.Finalizers = append(cluster.Finalizers, "test-finalizer")
+			k8sClient.Update(ctx, &cluster)
+			Eventually(func () bool {
+				k8sClient.Get(ctx, types.NamespacedName{
+					Namespace: "default",
+					Name:      name,
+				}, &cluster)
+				return len(cluster.Finalizers) == 2
+			}, timeout).Should(BeTrue())
+
+			k8sClient.Delete(ctx, &cluster)
+			Eventually(func () bool {
+				k8sClient.Get(ctx, types.NamespacedName{
+					Namespace: "default",
+					Name:      name,
+				}, &cluster)
+				return len(cluster.Finalizers) == 1 && cluster.Finalizers[0] == "test-finalizer"
+			}, timeout).Should(BeTrue())
 		})
 	})
 })
+
+func createAndBasicTest(ctx context.Context, name string) {
+	psp := &policyv1beta1.PodSecurityPolicy{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: name,
+		},
+		Spec: policyv1beta1.PodSecurityPolicySpec{
+			SELinux: policyv1beta1.SELinuxStrategyOptions{
+				Rule: policyv1beta1.SELinuxStrategyRunAsAny,
+			},
+			RunAsUser: policyv1beta1.RunAsUserStrategyOptions{
+				Rule: policyv1beta1.RunAsUserStrategyMustRunAsNonRoot,
+			},
+			SupplementalGroups: policyv1beta1.SupplementalGroupsStrategyOptions{
+				Rule: policyv1beta1.SupplementalGroupsStrategyRunAsAny,
+			},
+			FSGroup: policyv1beta1.FSGroupStrategyOptions{
+				Rule: policyv1beta1.FSGroupStrategyRunAsAny,
+			},
+		},
+	}
+	sparkCluster := &dcv1alpha1.SparkCluster{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: "default",
+		},
+		Spec: dcv1alpha1.SparkClusterSpec{
+			Image: &dcv1alpha1.OCIImageDefinition{
+				Repository: "foo",
+				Tag:        "bar",
+			},
+			Autoscaling: &dcv1alpha1.Autoscaling{
+				MinReplicas:           pointer.Int32Ptr(1),
+				MaxReplicas:           1,
+				AverageCPUUtilization: pointer.Int32Ptr(50),
+			},
+			NetworkPolicy: dcv1alpha1.SparkClusterNetworkPolicy{
+				Enabled: pointer.BoolPtr(true),
+			},
+			Worker: dcv1alpha1.SparkClusterWorker{
+				Replicas: pointer.Int32Ptr(1),
+			},
+			ClusterPort:       7077,
+			DashboardPort:     8265,
+			PodSecurityPolicy: psp.Name,
+		},
+	}
+
+	Expect(k8sClient.Create(ctx, psp)).To(Succeed())
+	Expect(k8sClient.Create(ctx, sparkCluster)).To(Succeed())
+}
