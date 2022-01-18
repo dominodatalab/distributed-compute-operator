b3bd1429e5a79ed84cc975aa089982eaf07a7904
Kevin Flansburg <66090878+ddl-kevin@users.noreply.github.com>
2022-01-18 16:37:09 +0000

    Add MPI client networkpolicy (#74)

--- pkg/cluster/mpi/networkpolicy.go
+++ pkg/cluster/mpi/networkpolicy.go
@@ -1,6 +1,7 @@
 package mpi
 
 import (
+	"errors"
 	"fmt"
 
 	networkingv1 "k8s.io/api/networking/v1"
@@ -13,40 +14,72 @@ import (
 	"github.com/dominodatalab/distributed-compute-operator/pkg/controller/core"
 )
 
-func NetworkPolicy() core.OwnedComponent {
-	return &networkPolicyComponent{}
+func WorkerNetworkPolicy() core.OwnedComponent {
+	return &networkPolicyComponent{
+		comp: ComponentWorker,
+	}
 }
 
-type networkPolicyComponent struct{}
+func DriverNetworkPolicy() core.OwnedComponent {
+	return &networkPolicyComponent{
+		comp: ComponentDriver,
+	}
+}
+
+type networkPolicyComponent struct {
+	comp metadata.Component
+}
 
 func (c networkPolicyComponent) Reconcile(ctx *core.Context) (ctrl.Result, error) {
 	cr := objToMPICluster(ctx.Object)
 
 	matchLabels := meta.MatchLabels(cr)
+
+	var podSelectorMatchLabels map[string]string
+	var ingressRules []networkingv1.NetworkPolicyPeer
+
+	switch c.comp {
+	case ComponentWorker:
+		podSelectorMatchLabels = matchLabels
+		ingressRules = []networkingv1.NetworkPolicyPeer{
+			{
+				PodSelector: &metav1.LabelSelector{
+					MatchLabels: matchLabels,
+				},
+			},
+			{
+				PodSelector: &metav1.LabelSelector{
+					MatchLabels: cr.Spec.NetworkPolicy.ClientLabels,
+				},
+			},
+		}
+	case ComponentDriver:
+		podSelectorMatchLabels = cr.Spec.NetworkPolicy.ClientLabels
+		ingressRules = []networkingv1.NetworkPolicyPeer{
+			{
+				PodSelector: &metav1.LabelSelector{
+					MatchLabels: matchLabels,
+				},
+			},
+		}
+	case metadata.ComponentNone:
+		err := errors.New("unknown component for NetworkPolicy")
+		return ctrl.Result{}, err
+	}
+
 	netpol := &networkingv1.NetworkPolicy{
 		ObjectMeta: metav1.ObjectMeta{
-			Name:      meta.InstanceName(cr, metadata.ComponentNone),
+			Name:      meta.InstanceName(cr, c.comp),
 			Namespace: cr.Namespace,
 			Labels:    meta.StandardLabels(cr),
 		},
 		Spec: networkingv1.NetworkPolicySpec{
 			PodSelector: metav1.LabelSelector{
-				MatchLabels: matchLabels,
+				MatchLabels: podSelectorMatchLabels,
 			},
 			Ingress: []networkingv1.NetworkPolicyIngressRule{
 				{
-					From: []networkingv1.NetworkPolicyPeer{
-						{
-							PodSelector: &metav1.LabelSelector{
-								MatchLabels: matchLabels,
-							},
-						},
-						{
-							PodSelector: &metav1.LabelSelector{
-								MatchLabels: cr.Spec.NetworkPolicy.ClientLabels,
-							},
-						},
-					},
+					From: ingressRules,
 				},
 			},
 			PolicyTypes: []networkingv1.PolicyType{
