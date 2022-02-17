85b1d37122494c9c7f093841fc743c312dde204b
Andrey Petrov <andrey.petrov@dominodatalab.com>
2022-02-17 16:40:14 -0600

    DOM-36136 Use generic rsyncd for file sync (#90)
    * DOM-36136 Sidecar image for rsyncd
    
    * DOM-36136 Install ps

--- dockerfiles/rsync-start.sh
+++ dockerfiles/rsync-start.sh
@@ -3,10 +3,9 @@
 set -o nounset
 set -o errexit
 
-CONFIG_DIR="/opt/domino/etc"
-
-/usr/bin/ssh-keygen -f "$CONFIG_DIR/ssh_host_key" -N '' -t ed25519
-chmod 400 "$CONFIG_DIR/ssh_host_key"
-
-/usr/sbin/sshd -f "$CONFIG_DIR/sshd_config" -o "Port $RSYNC_PORT" -De 2>&1 | \
-	grep -v 'kex_exchange_identification'
+/usr/bin/rsync \
+	--daemon \
+	--no-detach \
+	--verbose \
+       	--config="$DOMINO_ETC/$RSYNC_CONFIG_FILE" \
+	--port=$RSYNC_PORT
