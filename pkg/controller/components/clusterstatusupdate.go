f15642e0459905f1deeaa3ce83024e286c88b147
Andrey Petrov <andrey.petrov@dominodatalab.com>
2022-09-23 17:39:55 -0500

    Reposrting status of Dask cluster

--- pkg/controller/components/clusterstatusupdate.go
+++ pkg/controller/components/clusterstatusupdate.go
@@ -4,6 +4,10 @@ import (
 	"fmt"
 	"reflect"
 	"sort"
+	"strings"
+	"time"
+
+	v1 "k8s.io/api/batch/v1"
 
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
@@ -22,6 +26,8 @@ type ClusterStatusUpdateDataSource interface {
 	ClusterStatusConfig() *dcv1alpha1.ClusterStatusConfig
 	Image() *dcv1alpha1.OCIImageDefinition
 }
+
+const finalizerRetryPeriod = 1 * time.Second
 
 type ClusterStatusUpdateDataSourceFactory func(client.Object) ClusterStatusUpdateDataSource
 
@@ -47,8 +53,14 @@ func (c *clusterStatusUpdateComponent) Reconcile(ctx *core.Context) (ctrl.Result
 	}
 
 	var podNames []string
+	var masterPod corev1.Pod
+	masterPodFound := false
 	for _, pod := range podList.Items {
 		podNames = append(podNames, pod.Name)
+		if !masterPodFound && strings.Contains(pod.Name, "-scheduler") {
+			masterPodFound = true
+			masterPod = pod
+		}
 	}
 	sort.Strings(podNames)
 
@@ -87,10 +99,48 @@ func (c *clusterStatusUpdateComponent) Reconcile(ctx *core.Context) (ctrl.Result
 		modified = true
 	}
 
+	var status v1.JobConditionType
+	if masterPodFound {
+		if dcv1alpha1.IsPodReady(masterPod) {
+			status = dcv1alpha1.RunningStatus
+		} else {
+			status = dcv1alpha1.StartingStatus
+		}
+	} else {
+		status = dcv1alpha1.PendingStatus
+	}
+	if csc.ClusterStatus != status && ctx.Object.GetDeletionTimestamp() == nil {
+		modified = true
+		csc.ClusterStatus = status
+		if status == dcv1alpha1.RunningStatus {
+			tt := metav1.Now()
+			csc.StartTime = &tt
+		} else {
+			csc.StartTime = nil
+		}
+	}
+
 	// only update when fields have changed
 	if modified {
 		err = ctx.Client.Status().Update(ctx, ctx.Object)
 	}
 
 	return ctrl.Result{}, err
+}
+
+func (c clusterStatusUpdateComponent) Finalize(ctx *core.Context) (ctrl.Result, bool, error) {
+	ds := c.factory(ctx.Object)
+	csc := ds.ClusterStatusConfig()
+
+	if csc.ClusterStatus != dcv1alpha1.StoppingStatus {
+		csc.ClusterStatus = dcv1alpha1.StoppingStatus
+		csc.StartTime = nil
+		err := ctx.Client.Status().Update(ctx, ctx.Object)
+		if err != nil {
+			return ctrl.Result{RequeueAfter: finalizerRetryPeriod}, false,
+				fmt.Errorf("cannot update cluster status: %w", err)
+		}
+	}
+
+	return ctrl.Result{}, true, nil
 }
