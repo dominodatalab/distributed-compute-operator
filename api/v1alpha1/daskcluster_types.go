af7bad33cc9a09ba671b772d3f7947512c9d0a3d
Andrey Petrov <andrey.petrov@dominodatalab.com>
2022-09-23 16:28:05 -0500

    Report status in a column

--- api/v1alpha1/daskcluster_types.go
+++ api/v1alpha1/daskcluster_types.go
@@ -33,6 +33,7 @@ type DaskClusterStatus struct {
 //+kubebuilder:subresource:status
 //+kubebuilder:subresource:scale:specpath=.spec.worker.replicas,statuspath=.status.workerReplicas,selectorpath=.status.workerSelector
 //+kubebuilder:printcolumn:name="Workers",type=integer,JSONPath=".spec.worker.replicas"
+//+kubebuilder:printcolumn:name="Status",type=string,JSONPath=".status.clusterStatus"
 //+kubebuilder:printcolumn:name="Age",type=date,JSONPath=".metadata.creationTimestamp"
 //+kubebuilder:printcolumn:name="Image",type=string,JSONPath=".spec.image"
 //+kubebuilder:printcolumn:name="Network Policy",type=boolean,JSONPath=".spec.networkPolicy.enabled",priority=10
