ac44922dbf3d5c452ffc3515254dfda7cd234173
Andrey Petrov <andrey.petrov@dominodatalab.com>
2022-12-14 10:15:14 -0600

    HBD-766 • Hook up Dask cluster

--- controllers/daskcluster_controller.go
+++ controllers/daskcluster_controller.go
@@ -22,8 +22,10 @@ func DaskCluster(mgr ctrl.Manager, webhooksEnabled bool, cfg *Config) error {
 		Component("rolebinding-podsecuritypolicy", dask.RoleBindingPodSecurityPolicy()).
 		Component("service-scheduler", dask.ServiceScheduler()).
 		Component("service-worker", dask.ServiceWorker()).
+		Component("service-api-proxy", dask.APIProxyService()).
 		Component("networkpolicy-scheduler", dask.NetworkPolicyScheduler()).
 		Component("networkpolicy-worker", dask.NetworkPolicyWorker()).
+		Component("networkpolicy-api-proxy", dask.APIProxyNetworkPolicy()).
 		Component("statefulset-scheduler", dask.StatefulSetScheduler()).
 		Component("statefulset-worker", dask.StatefulSetWorker()).
 		Component("horizontalpodautoscaler", dask.HorizontalPodAutoscaler()).
---
+++ pkg/cluster/dask/apiproxy.go
@@ -0,0 +1,26 @@
+package dask
+
+import (
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	"github.com/dominodatalab/distributed-compute-operator/pkg/controller/components"
+	"github.com/dominodatalab/distributed-compute-operator/pkg/controller/core"
+)
+
+func APIProxyService() core.OwnedComponent {
+	return components.APIProxyServiceComponent{
+		APIProxyPort: func(obj *client.Object) int32 {
+			return daskCluster(*obj).Spec.APIProxyPort
+		},
+		Meta: meta,
+	}
+}
+
+func APIProxyNetworkPolicy() core.OwnedComponent {
+	return components.APIProxyNetworkPolicyComponent{
+		APIProxyPort: func(obj *client.Object) int32 {
+			return daskCluster(*obj).Spec.APIProxyPort
+		},
+		Meta: meta,
+	}
+}
